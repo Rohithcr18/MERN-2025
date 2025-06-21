1536e851ec7e843e56be539b1d64b64fe906bd0c
rohithrt1402@gmail.com <rohithrt1402@gmail.com>
2025-06-21 15:55:54 +0530

    five

--- react/src/Hooks/Ref.jsx
+++ react/src/Hooks/Ref.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 const Ref = () => {
+    const [count,setCount] = useState(0);
+    const value = useRef(0)
+    const handleIncrement = ()=>{
+        setCount(count+1)
+    }
+    useEffect(() =>{
+        //console.log("Count changed");
+    })
+    const handleValueIncrement = ()=>{
+        value.current=value.current+1;
+    }
   return (
-    <div>Ref</div>
+    <div>
+        <h1>{count}</h1>
+        <button onClick={handleIncrement}>increment</button>
+        <h1>{value.current}</h1>
+        <button onClick={handleValueIncrement}>increment value</button>
+    </div>
   )
 }
 
